66c159eec39bee38ccba6615544b23dd55b95fd0
unknown <jirizlamalik@icloud.com>
2020-04-23 23:41:42 +0200

    new corner

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,23 @@
 // VARIABLES
 let articleDivs = document.querySelectorAll(".article-container div");
+let album = document.querySelector('.fotoalbum');
+let curl = document.querySelector('.curl');
+
+
+curl.addEventListener('mouseenter',()=>{
+    album.classList.add('fotoalbum-changed');
+})
+
+curl.addEventListener('mouseleave',()=>{
+    album.classList.remove('fotoalbum-changed');
+})
+
+curl.addEventListener('click',()=>{
+    window.location.href = "album.html";
+})
+
+
+
 
 articleDivs.forEach(div =>{
 
@@ -7,3 +25,5 @@ articleDivs.forEach(div =>{
     div.onmouseout = ()=>{div.classList.remove("selected")};
 
 })
+
+
